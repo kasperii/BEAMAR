cd818bd19020f37db9c7e11d3cbef8af89494de6
Jonij001 <jonas.abunijmeh@gmail.com>
2018-09-25 17:50:49 +0200

    Closes point works and follows the player, started implementing the code on linerenderer
    Closes point works and follows the player, started implementing the code on linerenderer

--- Unity project/Assets/Scripts/ClosestPointSoundScript.cs
+++ Unity project/Assets/Scripts/ClosestPointSoundScript.cs
@@ -10,6 +10,8 @@ public class ClosestPointSoundScript : MonoBehaviour {
 	public GameObject Sphere;
 	public GameObject Cube2;
 
+	public LineRenderer Line1;
+
 	public float DistanceCamCube;
 	public float DistanceCamSphere;
 	public float DistanceCubeSphere;
@@ -21,21 +23,36 @@ public class ClosestPointSoundScript : MonoBehaviour {
 	public float Projectx;
 	public float Projecty;
 	public float Projectz;
+
+	public float angleToSphere;
+	public float angleToCube;
+
+	public float LineRenderPositions;
+	public float LineRenderPositionsx;
+	public float LineRenderPositionsy;
+	public float LineRenderPositionsz;
+
 	// Use this for initialization
 	void Start () {
-
+		Line1 = gameObject.GetComponent<LineRenderer>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-
-		DistanceCamCube = Vector3.Distance(FirstPersonCamera.transform.position,Cube.transform.position);
+	/*	DistanceCamCube = Vector3.Distance(FirstPersonCamera.transform.position,Cube.transform.position);
 		DistanceCamSphere = Vector3.Distance(FirstPersonCamera.transform.position,Sphere.transform.position);
-		DistanceCubeSphere = Vector3.Distance(Cube.transform.position,Sphere.transform.position);
+		DistanceCubeSphere = Vector3.Distance(Cube.transform.position,Sphere.transform.position);*/
 
+	/*	LineRenderPositions = Line1.GetPosition;
+		LineRenderPositionsx = LineRenderPositions.x;
+		LineRenderPositionsy = LineRenderPositions.y;
+		LineRenderPositionsz = LineRenderPositions.z; */
+
+		Vector3 VectorCamSphere = FirstPersonCamera.transform.position - Sphere.transform.position;
 		Vector3 VectorCamCube = FirstPersonCamera.transform.position - Cube.transform.position;
-		Vector3 VectorCubeSphere = Cube.transform.position - Sphere.transform.position;
+		Vector3 VectorCubeSphere = Sphere.transform.position - Cube.transform.position;
+
 		Vector3 Project =	Vector3.Project(VectorCamCube,VectorCubeSphere);
 		Project = Project+Cube.transform.position;
 
@@ -43,13 +60,19 @@ public class ClosestPointSoundScript : MonoBehaviour {
 		Projecty = Project.y;
 		Projectz = Project.z;
 
-		if (!GameObject.FindGameObjectWithTag("test_cube")){
-			var test_object = Instantiate(Cube2, Project, Quaternion.identity);
-		}
-		else {
-			//Vector3.MoveTowards()
-		}
+		angleToSphere = Vector3.Angle(VectorCamSphere, VectorCubeSphere);
+		angleToCube = Vector3.Angle(VectorCamCube, VectorCubeSphere);
 
+		if (angleToSphere>90 && angleToCube<90){
+			if (!GameObject.FindGameObjectWithTag("test_cube")){
+				var test_object = Instantiate(Cube2, Project, Quaternion.identity);
+			}
+			else {
+					GameObject.FindGameObjectWithTag("test_cube").transform.position = Project;
+				//Destroy(GameObject.FindGameObjectWithTag("test_cube"));
+				//var test_object = Instantiate(Cube2, Project, Quaternion.identity);
+			}
+		}
 
 
 	}
