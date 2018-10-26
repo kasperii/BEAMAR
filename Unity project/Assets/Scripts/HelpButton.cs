a99e76c74276377175d05985979cc7cf5677dea1
Erik Lindström <erik-lindstrom94@hotmail.com>
2018-10-26 10:57:46 +0200

    help canvas, bugfixes

---
+++ Unity project/Assets/Scripts/HelpButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpButton : MonoBehaviour {
+
+    [SerializeField] private GameObject HelpPage;
+
+    private bool activeOrNot = false;
+
+    public void OnClick() {
+        activeOrNot = !activeOrNot;
+        HelpPage.SetActive(activeOrNot);
+    }
+
+
+}
