bb9b302c64b3691fd49f25ca56407abb7f0a2139
MangoGott <denhemligafilmen@gmail.com>
2018-10-29 13:02:34 +0100

    LASER COOL
    the laser is fucking done BRO

--- Unity project/Assets/Scripts/SoundSource.cs
+++ Unity project/Assets/Scripts/SoundSource.cs
@@ -2,46 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundSource : MonoBehaviour {
+public class SoundSource : MonoBehaviour
+{
 
-	private Camera FirstPersonCamera;
-	private Transform cube2Trans;
+    private Camera FirstPersonCamera;
+    private Transform cylinder2Trans;
 
-	private Vector3[] LineIndex = null;
-	int u = 0;
-
-	public float angleToIndex1;
-	public float angleToIndex2;
+    private Vector3[] LineIndex = null;
+    int u = 0;
 
 	// Use this for initialization
-	void Start () {
-		FirstPersonCamera = GameObject.Find("FirstPersonCamera").GetComponent<Camera>();
-		cube2Trans = gameObject.GetComponent<Transform>();
-	}
-	// Update is called once per frame
-	void Update () {
+	void Start()
+    {
+        FirstPersonCamera = GameObject.Find("FirstPersonCamera").GetComponent<Camera>();
+        cylinder2Trans = gameObject.GetComponent<Transform>();
+    }
+    void Update()
+    {
 
-		//Debug.Log("Uu: " + u);
-		//Debug.Log("Xx: " + LineIndex[u-1].x + " Yy: " + LineIndex[u-1].y + " Zz: " + LineIndex[u-1].z);
-		Vector3 VectorIndex1 = FirstPersonCamera.transform.position - LineIndex[u-1];
-		Vector3 VectorIndex2 = FirstPersonCamera.transform.position - LineIndex[u];
-		Vector3 VectorIndextoIndex = LineIndex[u-1] - LineIndex[u]; //noteToSelf: jump over the first index in loop
 
-		Vector3 ProjectonLine =	Vector3.Project(VectorIndex2,VectorIndextoIndex);
-		ProjectonLine = ProjectonLine+LineIndex[u];
+        Vector3 VectorIndextoIndex = LineIndex[u - 1] - LineIndex[u]; //noteToSelf: jump over the first index in loop
+        float distanceVectors = VectorIndextoIndex.magnitude;
 
-		angleToIndex1 = Vector3.Angle(VectorIndex1, VectorIndextoIndex);
-		angleToIndex2 = Vector3.Angle(VectorIndex2, VectorIndextoIndex);
 
-		if (angleToIndex1>90 && angleToIndex2<90){
+        transform.localScale = new Vector3(0.1f, 0.1f, distanceVectors);
+        transform.position = LineIndex[u] + (VectorIndextoIndex/2f);
+        transform.LookAt(LineIndex[u - 1]);
+        //transform.LookAt(VectorIndextoIndex);
+        //transform.Rotate(9f, 9f, 9f, Space.Self);
 
-					cube2Trans.position = ProjectonLine;
-		}
-	}
 
-	public void InstantiateSoundSource(int uIn, Vector3[] LineIndexIn)
-	{
-		LineIndex = LineIndexIn;
-		u = uIn;
-	}
+    }
+
+    public void InstantiateSoundSource(int uIn, Vector3[] LineIndexIn)
+    {
+        LineIndex = LineIndexIn;
+        u = uIn;
+    }
+
 }
